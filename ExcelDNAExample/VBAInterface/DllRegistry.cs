70d23a148358fe1f3e774bed245fb45e1dfb466e
peter mahady <peter mahady>
2016-10-11 14:53:20 +0200

    commit the intial excel dna template to the repository

---
+++ ExcelDNAExample/VBAInterface/DllRegistry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ExcelDna.Integration;
+using ExcelDna.ComInterop;
+using ExcelDNAExample.Functions;
+using System.Runtime.InteropServices;
+//
+namespace ExcelVBAInterface
+{
+    //
+    [ComVisible(false)]
+    class ExcelAddin : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            ComServer.DllRegisterServer();
+        }
+        public void AutoClose()
+        {
+            ComServer.DllUnregisterServer();
+        }
+    }
+}
---
+++ ExcelDNAExample/VBAInterface/FunctionsWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ExcelDna.Integration;
+using ExcelDna.ComInterop;
+using ExcelDNAExample.Functions;
+using System.Runtime.InteropServices;
+
+//This class is a wrapper for the functions we already created
+//The reason we need it is because The functions exported as UDFs must be 'static',
+//while the COM-visible methods must be instance methods.
+namespace ExcelVBAInterface
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDispatch)]
+    [ProgId("CSharp_functionLibrary")]
+    public class FunctionsWrapper
+    {
+    	public string HelloSam()
+        {
+    		return Functions.Hello();
+        }  	
+    	
+    }
+}
